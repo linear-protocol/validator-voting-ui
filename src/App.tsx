b44b6d4157732b6e5b96f75f17b1c76b6d6becae
Jin <57248516+chickick@users.noreply.github.com>
2025-05-15 09:32:18 +0800

    chore: repo init & config (#1)
    Co-authored-by: Linguists <95207870+linguists@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Vote Now</Button>
+    </div>
+  );
+}
+
+export default App;
