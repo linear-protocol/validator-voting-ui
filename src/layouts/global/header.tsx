8b2a6b5a91771d3071f5a9ffdd280fe47401a9bc
jin <57248516+chickick@users.noreply.github.com>
2025-06-09 15:43:05 +0800

    feat: add github links

--- src/layouts/global/header.tsx
+++ src/layouts/global/header.tsx
@@ -1,5 +1,13 @@
+import { ChevronDown } from 'lucide-react';
+
 import NEARLogo from '@/assets/icons/near.svg?react';
+import GithubImg from '@/assets/images/github.png';
 import LinearImg from '@/assets/images/linear.png';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 export default function Header() {
   return (
@@ -7,12 +15,48 @@ export default function Header() {
       <div className="flex items-center">
         <NEARLogo height={30} />
       </div>
-      <a href="https://app.linearprotocol.org/" target="_blank" rel="noopener noreferrer">
-        <div className="flex items-center text-app-black-600 gap-3 text-base">
-          Powered by
-          <img src={LinearImg} className="h-6" />
-        </div>
-      </a>
+      <div className="flex items-center">
+        <Popover>
+          <PopoverTrigger asChild>
+            <div className="gap-1 flex items-center cursor-pointer">
+              <img src={GithubImg} className="h-6" />
+              <ChevronDown className="h-4" />
+            </div>
+          </PopoverTrigger>
+          <PopoverContent align="start" className="w-32">
+            <ul>
+              <li className="text-app-black-800 hover:text-app-black-600 mb-2">
+                <a
+                  href="https://github.com/linear-protocol/validator-voting-contract"
+                  target="_blank"
+                >
+                  Contract
+                </a>
+              </li>
+              <li className="text-app-black-800 hover:text-app-black-600 mb-2">
+                <a href="https://github.com/linear-protocol/validator-voting-ui" target="_blank">
+                  UI
+                </a>
+              </li>
+              <li className="text-app-black-800 hover:text-app-black-600">
+                <a
+                  href="https://github.com/linear-protocol/validator-voting-subgraph"
+                  target="_blank"
+                >
+                  Subgraph
+                </a>
+              </li>
+            </ul>
+          </PopoverContent>
+        </Popover>
+        <div className="bg-app-black-400 w-[1px] h-3 mx-2.5"></div>
+        <a href="https://app.linearprotocol.org/" target="_blank" rel="noopener noreferrer">
+          <div className="flex items-center text-app-black-600 gap-3 text-base">
+            <span className="hidden sm:inline">Powered by</span>
+            <img src={LinearImg} className="h-6" />
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
